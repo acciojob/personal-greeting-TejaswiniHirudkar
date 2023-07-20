c946346b5fc812bd1364ba0826381d12e6aee182
TejaswiniHirudkar <tejuhirudkar99@gmail.com>
2023-07-20 22:55:59 +0530

    submitting code-Thu Jul 20 2023 22:55:36 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,25 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [name, setName]=useState('');
+  const [greeting, setGreeting]=useState('')
+
+  const handleNamechange = (e)=>{
+    setName(e.target.value);
+  }
+
+  const handleGreeting = ()=>{
+    setGreeting('Hello ${name}!');
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+      <label>Enter your name:</label>
+      <input type='text' value={name} id='nameInput' onChange={handleNamechange}></input>
+      <p>{greeting}</p>
     </div>
   )
 }
